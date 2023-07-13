116b805075a34cde56f88ee8cc4c33c0e636caae
tokumotoshotaro001 <138739547+tokumotoshotaro001@users.noreply.github.com>
2023-07-13 15:01:35 +0900

    AppMain書いた
    AppMainの中身のFCとGCとICを書いた

--- CProgramTemplate1/CProgramTemplate1/AppMain.cpp
+++ CProgramTemplate1/CProgramTemplate1/AppMain.cpp
@@ -52,6 +52,53 @@ int WINAPI WinMain(_in_HINSTANCE hinstance, _In_opt_HINSTANCE hPrevInstance,
 	SetGraphMode(SCREEN_WIDTH, SCREEN_HEGHT, SCREEN_COLORBIT);
 	
 	//Dxライブラリ初期化処理
+	
+	//エラーが発生したら、終了する
+
+	if (SeneManager_Initialize(E_TITLE) == D_ERROR)
+	{
+		return D_ERROR;
+	}
+	
+	//各機能の初期化処理
+	FreamControl_Initiakize(); //フレームレート制御機能
+	Input_Initialize(); //入力制御機能
+
+	//シーンマネージャー初期化処理
 	//えらーがはっせいしたら、終了する
+	if (SceneManager_Initialize(E_TITLE) == D_ERROR)
+	{
+		return D_ERROR
+	}
+
+	//描画先画面を裏にする
+	SetFontSize(FONT_SIZE);
+
+	//ゲームループ
+	while (ProcessMessage() != D_ERROR && input_Escape() == FALSE)
+	{
+		//入力制御機能更新処理
+		Input_Upsate();
+
+		//シーンマネージャー更新処理
+		SceneManager_Updata();
+
+		//画面クリア
+		ClearDrawScreen();
+
+		//シーンマネージャー描画処理
+		SceneManager_Draw();
+
+		//フレームレート制御処理
+		FreamControl_Update();
+
+		//画面の内容を表画面に反映
+		screenFlip();
+	}
+	//Dxライブラリ使用の終了処理
+	DxLib_end();
+
+	return 0;
 
 }
+
---
+++ CProgramTemplate1/CProgramTemplate1/InputControl.h
@@ -0,0 +1,25 @@
+#pragma once
+
+/************************
+* マクロ定義
+************************/
+
+/************************
+* 型定義
+************************/
+
+
+/************************
+* プロトタイプ宣言
+************************/
+
+void Input_Initialize(void);
+void Input_Update(void);
+void Input_Escape(void);
+
+int GetOldKey(int key);
+int GetNowKey(int key);
+int GetKeyFlg(int key);
+int GetMousePositionX(void);
+int GetMousePositionY(void);
+
