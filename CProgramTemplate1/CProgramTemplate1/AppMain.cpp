8229c9021c4ce22810a7c2f59df6c44ccd2259e7
tokumotoshotaro001 <138739547+tokumotoshotaro001@users.noreply.github.com>
2023-07-13 12:09:37 +0900

    AppMain書いた
    AppMainの中身書いた

--- CProgramTemplate1/CProgramTemplate1/AppMain.cpp
+++ CProgramTemplate1/CProgramTemplate1/AppMain.cpp
@@ -1,27 +1,57 @@
+/*********************************
+*プログラミング実習
+*マッチ3ゲームの製作
+*********************************/
 #include "DxLib.h"
 
-int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPprevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+#include "FreamControl.h"
+
+#include "InputControl.h"
+
+#include "SceneManager.h"
+
+/********************************
+*マクロ定義
+********************************/
+
+#define SCREEN_HEIGHT (480) //スクリーンサイズ(高さ)
+
+#define SCREEN_HEIGHT (480) //スクリーンサイズ(高さ)
+
+#define SCREEN_COLORBIT (32) //スクリーンカラービット
+
+#define FONT_SIZE (32) //文字サイズ
+
+/********************************
+*型定義
+********************************/
+
+/********************************
+*グローバル変数宣言
+********************************/
+
+/********************************
+*プロトタイプ宣言
+********************************/
+
+/********************************
+*プログラムの開始
+********************************/
+
+int WINAPI WinMain(_in_HINSTANCE hinstance, _In_opt_HINSTANCE hPrevInstance,
+	_In_LPSTR lpCmdLine, _In_int nShowCmd)
+
 {
+	//ウィンドウタイトル設定
+	SetMainWindowText("Match 3 Puzzle");
+
 	//ウィンドウモードで起動
 	ChangeWindowMode(TRUE);
 
+	//画面サイズの最大サイズ、カラービット数を設定
+	SetGraphMode(SCREEN_WIDTH, SCREEN_HEGHT, SCREEN_COLORBIT);
+	
+	//Dxライブラリ初期化処理
+	//えらーがはっせいしたら、終了する
 
-	//Dxライブラリーの初期化処理
-
-	if (DxLib_Init() == -1)
-
-	{
-
-		return -1;
-	}
-
-
-	//入力待ち
-
-	WaitKey();
-
-	DxLib_End();
-
-
-	return 0;
 }
